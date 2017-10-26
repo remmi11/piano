6178c58abb3481c92452d86ad74f957f9bb20695
remmi11 <wtgeographer@gmail.com>
2017-10-26 12:22:14 -0500

    update before deploy

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import os
-from flask_openshift_template import app as application
-
-
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    httpd.serve_forever()
+ 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+	execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+	pass
+ 
+from run import app as application
